a90cd48f286c9efb87c4bb42520a75fc2c6c45c9
Vladislav_Kukushkin <vldkuku@gmail.com>
2024-05-29 23:04:38 +0300

    refactor vsego

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,85 +1,58 @@
-import test, { PlaywrightTestConfig, defineConfig, devices, expect } from '@playwright/test';
-import { describe } from 'node:test';
-//import { testPlanFilter } from "allure-playwright/dist/testplan";
-import { qase } from 'playwright-qase-reporter';
+import { PlaywrightTestConfig, devices } from '@playwright/test';
 
-describe('Test suite', () => {
-  test('Simple test', () => {
-    qase.id(1);
-    qase.title('Example of simple test')
-    expect(true).toBe(true);
-  });
-
-  test('Test with annotated fields', () => {
-    qase.id(2);
-    qase.fields({ 'severity': 'high', 'priority': 'medium' })
-    expect(true).toBe(true);
-  });
-
-  test(qase(2, 'This syntax is still supported'), () => {
-    expect(true).toBe(true);
-  });
-});
-
-export default defineConfig({
+const config: PlaywrightTestConfig = {
   testDir: './tests',
-
   fullyParallel: true,
-
   forbidOnly: !!process.env.CI,
-
   retries: process.env.CI ? 2 : 0,
-
   workers: process.env.CI ? 1 : undefined,
-
-  //grep: testPlanFilter(),
   use: {
     trace: 'on-first-retry',
     storageState: "./auth-lk.json",
     screenshot: "on",
-    video:"on",
+    video: "on",
     actionTimeout: 0,
-    },
+  },
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'],
-      storageState: "./auth-lk.json", 
-      
+      use: {
+        ...devices['Desktop Chrome'],
+      },
     },
-
-    },
-
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'],
-      storageState: "./auth-lk.json",
-     }
+      use: {
+        ...devices['Desktop Firefox'],
+      },
     },
-
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'],
-      storageState: "./auth-lk.json",
+      use: {
+        ...devices['Desktop Safari'],
       },
     },
   ],
-
-  
-});
-const config: PlaywrightTestConfig = {
   reporter: [
     ['list'],
-    ['playwright-qase-reporter',
-        {
-            apiToken: '65f07f630ba4928f3b9dfec902b155b7587609b2141a7b9f5376bb1c7b87eef7',
-            projectCode: 'DEMO',
-            basePath: 'https://api.qase.io/v1',
-            uploadAttachments: true,
-            runComplete: true,
-            logging: true,
-            rootSuiteTitle: 'Playwright tests',
-            environmentId: '1'
-        }],
-      ],};
-      module.exports = config;
+    [
+      'playwright-qase-reporter',
+      {
+        debug: true,
+        testops: {
+          api: {
+            token: '65f07f630ba4928f3b9dfec902b155b7587609b2141a7b9f5376bb1c7b87eef7',
+          },
+          project: 'DEMO',
+          uploadAttachments: true,
+          logging: true,
+          run: {
+            complete: true,
+          },
+        },
+      },
+    ],
+  ],
+};
+
+export default config;
