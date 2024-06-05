89b481fb2dc51ef36b938a000467dc110f5e4ba0
Vladislav_Kukushkin <vldkuku@gmail.com>
2024-06-05 18:47:11 +0300

    feat:added tests for payments

--- tests/prod-single-order-bitcoin-e2e.spec.ts
+++ tests/prod-single-order-bitcoin-e2e.spec.ts
@@ -9,8 +9,8 @@ test('test', async ({ page }) => {
   await page.goto('https://smmpanelka.com/app/orders/create?serviceSlug&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DF0pOnKmv0tw&quantity=1000&networkId=1&serviceTypeId=1&serviceId=157044&isDripFeed=false&dripFeedQuantityStep=1&numberOfRuns=2&dripFeedMode=flat&dripFeedMinutes=60&type=single');
   await page.getByRole('button', { name: 'Buy in one click' }).click({ timeout: 0 });
   await page.getByRole('button', { name: 'RUB' }).click({ timeout: 0 });
-  await page.getByText('Credit / Debit card').click({ timeout: 0 });
+  await page.getByText('Bitcoin').click({ timeout: 0 });
   await page.getByRole('button', { name: 'BUY FOR $' }).click({ timeout: 0 });
-  await page.goto('https://checkout.stripe.com/c/pay/cs_live_a1fVxpEGwOwgPLc1GFZYAsdjo4PxbWAjT8laWnymh8OY4GZEYFdsxpwOjt#fidkdWxOYHwnPyd1blppbHNgWjA0SE9ha31GMlBEQktNQDJ8c3B%2FQ2RrZ2JjYTZIamBObnw8bzRVZDdSSDFvVmFOMHZJd2dHU09XYktnaUozSGh8a3BrUHRTfTNVfF9xPXNuSkJBQWRJcGpWNTVzTndhNEBHbCcpJ2hsYXYnP34nYnBsYSc%2FJ0tEJyknaHBsYSc%2FJ0tEJykndmxhJz8nS0QneCknZ2BxZHYnP15YKSdpZHxqcHFRfHVgJz8ndmxrYmlgWmxxYGgnKSd3YGNgd3dgd0p3bGJsayc%2FJ21xcXV2PyoqdmhodWRrYGluZCtmamgneCUl');
-  await expect(page.getByText('Balance Replenishment for')).toBeVisible({ timeout: 0 });
+  await page.goto('https://gateway.gear.mycelium.com/pay/32e888bf0353b3f732098d96173dd0091ac5a1de29ba69106a7b3a275add774f?');
+  await expect(page.getByText('Tap or scan to pay').first()).toBeVisible();
 });
