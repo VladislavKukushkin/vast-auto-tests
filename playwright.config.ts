1422bd19a436eb0be9d26a0caceee8fea97ec39e
vdsavsdsdv vsdvdsvvdq <vladislav.kukushkin@onlinesup.com>
2024-07-15 11:09:12 +0300

    changed workers = 1

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig ({
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 0 : 0,
-  workers: process.env.CI ? 2 : undefined,
+  workers: process.env.CI ? 1 : undefined,
   grep: testPlanFilter(),
   reporter: [["line"], ["allure-playwright"]],
   use: {
